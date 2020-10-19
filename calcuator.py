c5ed44c51e059c40fb9f1a9f287cdb814fd48f20
SoulBreather123 <71325255+SoulBreather123@users.noreply.github.com>
2020-10-20 00:11:52 +0300

    Update calcuator.py

--- calcuator.py
+++ calcuator.py
@@ -1,12 +1,31 @@
-//WELCOME TO MY CALCUATOR!!
-print("1) Addition")
-print("2) Subtraction")
-print("3) Multiplication")
-print("4) Dividing")
+
+import random
+
+
+
+listContent1 = ["1) Addition", "2) Subtraction", "3) Multiplication", "4) Dividing",
+"5) Random Number Generator (1-100)"]
+
+releaseIsset1 = {"v0"}
+releaseIsset1.update("v2","congs")
+
+print(listContent1[0])
+print(listContent1[1])
+print(listContent1[2])
+print(listContent1[3])
+print(listContent1[4])
+
+
+
+
+
+
+
 
 
 
 selectOne = int(input("Select One:"))
+
 if selectOne == 1:
     xa = int(input("Write First Number: "))
     xaa = int(input("Write Second Number: "))
@@ -30,6 +49,17 @@ if selectOne == 4:
     xdd = int(input("Write Second Number: "))
     resultDividing1 = int(xd) / int(xdd)
     print("Result: ", resultDividing1)
+
+if selectOne == 5:
+    print("Random Number is:", random.randrange(1, 100))
+
+
+
+
+
+
+
+
 
 
 
